04bf1a2388dffe7e50d08931e0be7f87a1eeb8ca
shrushti425 <smraut3434@gmail.com>
2024-09-10 23:39:03 +0530

    Almost theree

--- sourcecode/part1.py
+++ sourcecode/part1.py
@@ -1,4 +1,4 @@
-import smtplib
+import getpass
 while(1>0):
     print("\nWelcome Aboard!\n\nEvery flight has a special experience and moment associated with it and we at SushiAir make it Remarkable for you!\n")
     users_fullnames=[]
@@ -14,25 +14,20 @@ while(1>0):
         name=input('Enter your full name\n')
         users_fullnames.append(name)
         print("Alright! Your account is almost ready let's secure it with a password\nThe password must contain:\n#Atleast one Special Character\n#Atleast one number\n#Atleast one UpperCase alphabet\n")
-        passkey=getpass.getpass('Password Please:\n')
-
-        if '!@#$%^&*-()_+=`~[]}{;:"?.><' in passkey:
-            c=1
-        elif '1234567890' in passkey:
-            d=1
-        elif 'QWERTYUIOPASDFGHJKLZXCVBNM' in passkey:
-            e=1
-
-        if c!=1:
-            print('Add a SPECIAL CHARACTER to the password!\n')
-        if d!=1:
-            print('Add a NUMBER to the password!\n')
-        if e!=1:
-            print('Add an UPPER CASE ALPHABET to the password!\n')
-        if c==1 and d==1 and e==1:
-            print("Your account has been created!\n")
-            userpasswords.append(passkey)
-            a+=1
+        x=0
+        while(x!=1):
+            passkey=getpass.getpass('Password Please:\n')
+             
+        
+            if '!@#$%^&*-()_+=`~[]}{;:"?.><' in passkey and '1234567890' in passkey and 'QWERTYUIOPASDFGHJKLZXCVBNM' in passkey:
+                print("Your account has been created!\n")
+                userpasswords.append(passkey)
+                a+=1
+                x=1
+            else:
+                print('Weak Password:\nCheck,Does your password contain:\n#A special character\n#A number\n#An uppercase character')
+                print('Try Again!\n')       
+        
         b=int(input(print('Enter 2 to Login!')))
     if b==2:
         name=input('Enter your email id')
